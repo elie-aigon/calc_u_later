d77f070da5a7451f7dc8563e7b22e3426085b7c4
Elie <elie.aigon@laplateforme.io>
2023-06-06 14:41:47 +0200

    mylan inspect

--- src/main/java/com/example/calc_u_later/Controller.java
+++ src/main/java/com/example/calc_u_later/Controller.java
@@ -2,6 +2,7 @@ package com.example.calc_u_later;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
@@ -11,14 +12,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    ObservableList<String> Units = FXCollections.observableArrayList("Centimètres", "Mètres", "Pouces", "Pieds");
+
     @FXML
-    private ComboBox<String> fromCombobox;
+    public ComboBox<String> ConvertFromCbb;
     @FXML
-    private ComboBox<String> toCombobox;
+    public ComboBox<String> ConvertToCbb;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        fromCombobox.setItems(Units);
-        toCombobox.setItems(Units);
+
     }
 }
