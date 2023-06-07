881ac643a864fbfceb9afd84a4c651d4ca6855f0
Elie <elie.aigon@laplateforme.io>
2023-06-07 03:46:17 +0200

    tapbane organize, calculator front begin

--- src/main/java/com/example/calc_u_later/App.java
+++ src/main/java/com/example/calc_u_later/App.java
@@ -2,7 +2,10 @@ package com.example.calc_u_later;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,8 +16,19 @@ import java.util.ResourceBundle;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("ConverterView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        TabPane tabPane = new TabPane();
+        Node CalculatorContent = FXMLLoader.load(App.class.getResource("CalculatorTab.fxml"));
+        Tab CalculatorTab = new Tab("Calculator");
+        CalculatorTab.setContent(CalculatorContent);
+        CalculatorTab.setClosable(false);
+        tabPane.getTabs().add(CalculatorTab);
+
+        Node ConverterContent = FXMLLoader.load(App.class.getResource("ConverterTab.fxml"));
+        Tab ConverterTab = new Tab("Converter");
+        ConverterTab.setContent(ConverterContent);
+        ConverterTab.setClosable(false);
+        tabPane.getTabs().add(ConverterTab);
+        Scene scene = new Scene(tabPane);
         stage.setTitle("Calc_U_later");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/calc_u_later/Controllers/CalculatorController.java
+++ src/main/java/com/example/calc_u_later/Controllers/CalculatorController.java
@@ -1,7 +1,6 @@
 package com.example.calc_u_later.Controllers;
 
 import javafx.fxml.Initializable;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
