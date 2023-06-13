cccd90ca7724addaf71fe5f08340335bdfefbbb5
pelayo-joel <joel.pelayo@laplateforme.io>
2023-06-13 15:23:41 +0200

    [Add] Init calculator branch

--- src/main/java/com/example/calc_u_later/controllers/CalculatorController.java
+++ src/main/java/com/example/calc_u_later/controllers/CalculatorController.java
@@ -4,7 +4,7 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-//import com.example.calc_u_later.models.calculator.*;
+//import com.example.calc_u_later.models.calculator.CalculatorModel;
 
 public class CalculatorController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
---
+++ src/main/java/com/example/calc_u_later/models/calculator/CalculatorModel.java
@@ -0,0 +1,7 @@
+package com.example.calc_u_later.models.calculator;
+
+/**
+ * Calculator's logic class
+ */
+public class CalculatorModel {
+}
