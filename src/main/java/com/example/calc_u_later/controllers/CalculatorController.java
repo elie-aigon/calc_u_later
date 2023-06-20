b093d5b3b19726b2c5238bdcb6c1f5142d5e946c
pelayo-joel <joel.pelayo@laplateforme.io>
2023-06-20 15:46:42 +0200

    [Add] Init base_calculator branch

--- src/main/java/com/example/calc_u_later/controllers/CalculatorController.java
+++ src/main/java/com/example/calc_u_later/controllers/CalculatorController.java
@@ -1,13 +1,32 @@
 package com.example.calc_u_later.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 //import com.example.calc_u_later.models.calculator.CalculatorModel;
 
 public class CalculatorController implements Initializable {
+    @FXML private Label inputExpr;
+    @FXML private Label inputValue;
+
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("a");
+    }
+
+    EventHandler<ActionEvent> buttonEvent() {
+        return event -> {
+            Button valueButton = (Button) event.getTarget();
+            String buttonLabel = valueButton.getText();
+
+            switch (buttonLabel) {
+                case "=":
+            }
+        };
     }
 }
